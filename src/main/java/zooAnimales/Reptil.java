282cc2d461d2890f994403c1233e042f53b0e08d
HinaraSM <69545933+HinaraSM@users.noreply.github.com>
2022-09-25 23:32:17 -0500

    Update Reptil.java

--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -10,7 +10,6 @@ public class Reptil extends Animal{
     public static int totalReptil;
 
     public Reptil() {
-        this(null, 0, null, null, null, 0);
     }  
     
     public Reptil(String nombre, int edad, String habitat, String genero,
@@ -18,7 +17,6 @@ public class Reptil extends Animal{
         super(nombre, edad, habitat, genero);
         this.colorEscamas = colorEscamas;
         this.largoCola = largoCola;
-        listado.add(this);
     }
 
     public static Reptil crearIguana (String nombre, int edad, String genero) {
