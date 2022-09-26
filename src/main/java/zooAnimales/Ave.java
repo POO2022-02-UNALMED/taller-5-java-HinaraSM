babc71b5e707bc7c1232a3e24b61d6c7c2da7723
HinaraSM <69545933+HinaraSM@users.noreply.github.com>
2022-09-25 23:30:50 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -15,7 +15,6 @@ public class Ave extends Animal {
             String colorPlumas) {
         super(nombre, edad, habitat, genero);
         this.colorPlumas = colorPlumas;
-        listado.add(this);
     }
     
     public static Ave crearHalcon(String nombre, int edad, String genero) {
